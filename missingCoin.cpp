ac8114134ce2f9bf5d0d0054c300fec3c90f7b76
Sakol Bun <112042178+SakolB@users.noreply.github.com>
2023-08-09 12:00:46 -0700

    Added detail explanation for missing coin solution

--- missingCoin.cpp
+++ missingCoin.cpp
@@ -15,6 +15,10 @@ int main()
     long long min = 1;
     //this works because if min is greater than a[i] then there is a gap
     //between the sorted array
+    //given a sorted number inside an array
+    //the minimum number it cannot produce is the number in the array gap
+    //after position i
+    //that is equal to 1 + sum(a[0] to a[i])
     for(int i = 0; i < n && a[i] <= min; i++)
     {
         min = min + a[i];
